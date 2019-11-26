8bf15efd665fe292da5c44ea2df9b2d54f0cf777
skippi <jbtcao@gmail.com>
2019-11-26 16:16:43 -0600

    Added DB file customization as CLI arg

--- standup/shell.py
+++ standup/shell.py
@@ -3,19 +3,31 @@
 import os
 import sys
 
+import click
 from standup.bot import BOT
 from standup.persist import DB, migrate
 
 
-def main():
-    """Entry point for `standup` script."""
+@click.command(options_metavar="[options]")
+@click.option(
+    "-db",
+    "--database",
+    "db_name",
+    nargs=1,
+    help="sqlite database file",
+    metavar="<database>",
+    default="repo.db",
+    show_default=True,
+)
+def main(db_name: str):
+    """Starts the standup bot."""
 
     token = os.getenv("STANDUP_BOT_TOKEN")
     if not token:
         print(f"standup: invalid token `{token if token else ''}`")
         sys.exit(1)
 
-    DB.init("repo.db")
+    DB.init(db_name)
     DB.connect()
     migrate(DB)
 
