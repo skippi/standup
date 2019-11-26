f8bbaa3e0039aac6922d3bdaa956363e3de36369
skippi <jbtcao@gmail.com>
2019-11-25 23:54:41 -0600

    Migrated persistance to peewee sqlite
    - Ditched pickle.
    - Adjusted CI and docker to support sqlite.
    - Adjusted existing statements to use sqlite.

--- standup/bot.py
+++ standup/bot.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta
 import discord
 from discord.ext import commands
 from standup import persist, post
+from standup.persist import Post, Room
 
 
 STANDUP_DM_HELP = """Please format your standup correctly, here is a template example: ```
@@ -41,12 +42,11 @@ async def on_command_error(ctx: commands.Context, exception):
 async def on_message(msg: discord.Message):
     await BOT.process_commands(msg)
 
-    rooms = persist.load_rooms()
-    related_room = next((r for r in rooms if r.channel_id == msg.channel.id), None)
+    related_room = Room.select().where(Room.channel_id == msg.channel.id).first()
     if not related_room:
         return
 
-    if post.message_is_formatted(msg.content):
+    if not post.message_is_formatted(msg.content):
         await msg.delete()
         await msg.author.send(STANDUP_DM_HELP)
         return
@@ -55,16 +55,12 @@ async def on_message(msg: discord.Message):
     roles_to_add = [msg.guild.get_role(id) for id in related_room.role_ids]
     await member.add_roles(*roles_to_add)
 
-    posts = persist.load_posts()
-    posts.append(
-        persist.Post(
-            channel_id=msg.channel.id,
-            user_id=msg.author.id,
-            role_ids=related_room.role_ids,
-            timestamp=datetime.now(),
-        )
+    Post.create(
+        channel_id=msg.channel.id,
+        user_id=msg.author.id,
+        role_ids=related_room.role_ids,
+        timestamp=datetime.now(),
     )
-    persist.save_posts(posts)
 
 
 @BOT.group()
@@ -76,28 +72,24 @@ async def rooms(ctx: commands.Context):
 @rooms.command(name="add")
 @commands.has_permissions(administrator=True)
 async def rooms_add(ctx: commands.Context, channel_id: int):
-    rooms = persist.load_rooms()
-    conflicting_rooms = [r for r in rooms if r.channel_id == channel_id]
-    if conflicting_rooms:
+    conflicting_room = Room.select().where(Room.channel_id == channel_id).first()
+    if conflicting_room:
         await ctx.send(f"```Failed: channel '{channel_id}' already is a room.```")
         raise commands.CommandError()
 
-    rooms.append(persist.Room(channel_id=channel_id, role_ids=set()))
-    persist.save_rooms(rooms)
+    Room.create(channel_id=channel_id, role_ids=set())
 
 
 @rooms.command(name="remove")
 @commands.has_permissions(administrator=True)
 async def rooms_remove(_, channel_id: int):
-    rooms = persist.load_rooms()
-    new_rooms = [r for r in rooms if r.channel_id != channel_id]
-    persist.save_rooms(new_rooms)
+    Room.delete().where(Room.channel_id == channel_id).execute()
 
 
 @rooms.command(name="list")
 @commands.has_permissions(administrator=True)
 async def rooms_list(ctx: commands.Context):
-    rooms = persist.load_rooms()
+    rooms = Room.select()
     rooms_formatted = [
         f"{index}: {r.channel_id} | Roles: {str(r.role_ids) if r.role_ids else '{}'}"
         for index, r in enumerate(rooms, 1)
@@ -110,8 +102,7 @@ async def rooms_list(ctx: commands.Context):
 @rooms.command(name="config")
 @commands.has_permissions(administrator=True)
 async def rooms_config(ctx: commands.Context, room: int, key: str, value: str):
-    rooms = persist.load_rooms()
-    target_room = next((r for r in rooms if r.channel_id == room), None)
+    target_room = Room.select().where(Room.channel_id == room).first()
     if not target_room:
         ctx.send(f"```\nFailed: room '{room}' does not exist.```")
         raise commands.CommandError()
@@ -120,8 +111,11 @@ async def rooms_config(ctx: commands.Context, room: int, key: str, value: str):
         role_ids = [int(s) for s in value.split(",")]
         filtered = [id for id in role_ids if ctx.guild.get_role(id)]
 
-        target_room.role_ids = set(filtered)
-        persist.save_rooms(rooms)
+        (
+            Room.update({Room.role_ids: Room.role_ids.db_value(set(filtered))})
+            .where(Room.channel_id == room)
+            .execute()
+        )
 
 
 async def prune_expired_posts_task():
@@ -130,11 +124,10 @@ async def prune_expired_posts_task():
     while not BOT.is_closed():
         await asyncio.sleep(60)
 
-        posts = persist.load_posts()
-        expired_posts, remaining_posts = _span(
-            posts, lambda p: datetime.now() - p.timestamp >= timedelta(hours=24)
+        expired_posts = Post.select().where(
+            Post.timestamp < (datetime.now() - timedelta(hours=24))
         )
-        if not expired_posts:
+        if len(expired_posts) == 0:
             continue
 
         for post in expired_posts:
@@ -144,19 +137,7 @@ async def prune_expired_posts_task():
             roles_to_remove = [guild.get_role(id) for id in post.role_ids]
             await member.remove_roles(*roles_to_remove)
 
-        persist.save_posts(remaining_posts)
+            post.delete_instance()
 
 
 BOT.loop.create_task(prune_expired_posts_task())
-
-
-def _span(lst, pred):
-    truthy = []
-    rest = []
-    for e in lst:
-        if pred(e):
-            truthy.append(e)
-        else:
-            rest.append(e)
-
-    return (truthy, rest)
--- standup/persist.py
+++ standup/persist.py
@@ -1,72 +1,67 @@
 """Module for persisting data."""
 
-import pickle
-from dataclasses import dataclass
-from datetime import datetime
-from typing import List, Set
+from typing import Any
+
+from peewee import CharField, DateTimeField, Model, SqliteDatabase, Field
 
 
-ROOMS_FILE = "rooms.pickle"
-POSTS_FILE = "posts.pickle"
+DB = SqliteDatabase("repo.db")
 
 
-@dataclass
-class Post:
+class SnowflakeField(CharField):
+    def db_value(self, value: Any) -> Any:
+        if isinstance(value, int):
+            return str(value)
+
+        return value
+
+    def python_value(self, value: Any) -> Any:
+        if isinstance(value, str):
+            return int(value)
+
+        return value
+
+
+class RoleSetField(Field):
+    def db_value(self, value: Any) -> Any:
+        if isinstance(value, set):
+            return ",".join(str(i) for i in value)
+
+        return value
+
+    def python_value(self, value: Any) -> Any:
+        if isinstance(value, str):
+            return set(int(s) for s in value.split(",") if s)
+
+        return value
+
+
+class BaseModel(Model):
+    class Meta:
+        database = DB
+
+
+class Post(BaseModel):
     """
     Represents an active standup post.
 
     Posts are active if their age is less than 24 hours.
     """
 
-    channel_id: int
-    user_id: int
-    role_ids: Set[int]
-    timestamp: datetime
+    channel_id = SnowflakeField()
+    user_id = SnowflakeField()
+    role_ids = RoleSetField()
+    timestamp = DateTimeField()
 
 
-def load_posts() -> List[Post]:
-    """Returns the list of active posts."""
-
-    try:
-        with open(POSTS_FILE, "r+b") as file:
-            return pickle.load(file)
-    except FileNotFoundError:
-        return []
-    except EOFError:
-        return []
-
-
-def save_posts(posts: List[Post]):
-    """Updates the list of active posts."""
-
-    with open(POSTS_FILE, "wb") as file:
-        pickle.dump(posts, file)
-
-
-@dataclass
-class Room:
+class Room(BaseModel):
     """
     Represents a standup channel.
     """
 
-    channel_id: int
-    role_ids: Set[int]
+    channel_id = SnowflakeField()
+    role_ids = RoleSetField()
 
 
-def load_rooms() -> List[Room]:
-    """Returns the list of active rooms."""
-
-    try:
-        with open(ROOMS_FILE, "r+b") as file:
-            return pickle.load(file)
-    except FileNotFoundError:
-        return []
-    except EOFError:
-        return []
-
-
-def save_rooms(rooms: List[Room]):
-    """Updates the list of active rooms."""
-
-    with open(ROOMS_FILE, "wb") as file:
-        pickle.dump(rooms, file)
+DB.connect()
+DB.create_tables([Post, Room])
